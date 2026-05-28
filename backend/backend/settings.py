b417949a0c9f2ee69046d0d2da6d1da3dd144938
Deepak-ai369 <adhikarideepak602@gmail.com>
2026-05-28 20:17:11 +0530

    Fix deployment config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@$ev8qv9eogwt_kn9&s9wwgsdz_efvq4r##=er_t36*#5z=smc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,3 +124,4 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
+CORS_ALLOW_ALL_ORIGINS = True
